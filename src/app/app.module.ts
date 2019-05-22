b2dc5cdc0085b6467644325edcf9861924e170c9
Jonathan Seed <jonathan.j.seed@gmail.com>
2019-05-22 10:47:07 -0400

    Move material module imports to separate module
    Simplifies imports for testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -15,9 +15,7 @@ import { HeaderComponent } from './header/header.component';
     BrowserModule,
     BrowserAnimationsModule,
 
-    // Material Imports
-    MatButtonModule,
-    MatToolbarModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
