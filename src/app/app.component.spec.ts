a3a3dfcbb89911f8affe7a80d93ab68925275182
Jonathan Seed <jonathan.j.seed@gmail.com>
2019-05-22 11:11:57 -0400

    Add basic form for selecting theme colors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,16 +2,23 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MaterialModule } from './material/material.module';
+import { ThemeBuilderComponent } from './theme-builder/theme-builder.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        CommonModule,
+        FormsModule,
         MaterialModule,
+        ReactiveFormsModule,
       ],
       declarations: [
         AppComponent,
         HeaderComponent,
+        ThemeBuilderComponent,
       ],
     }).compileComponents();
   }));
