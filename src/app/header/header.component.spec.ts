0272e06f7c39696f44b549316c3e66c70c9fe82a
Jonathan Seed <jonathan.j.seed@gmail.com>
2019-05-22 20:27:15 -0400

    Add button to header to preview accent and warn colors

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
-import { MatButtonModule, MatToolbarModule } from '@angular/material';
 import { MaterialModule } from '../material/material.module';
 
 describe('HeaderComponent', () => {
