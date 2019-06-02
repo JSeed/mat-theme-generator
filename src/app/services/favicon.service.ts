bd0698fbfb4e4befb6661cb97bc825cb5927a487
Jonathan Seed <jonathan.j.seed@gmail.com>
2019-06-02 12:30:57 -0400

    Dynamically update favicon to match theme

---
+++ src/app/services/favicon.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FaviconService {
+ 
+
+    private favicon: HTMLLinkElement
+    private canvas: HTMLCanvasElement;
+    private img: HTMLImageElement;
+    private context: CanvasRenderingContext2D;
+
+    constructor(
+        @Inject(DOCUMENT) private document: Document
+    ) {
+        this.favicon = <HTMLLinkElement>this.document.getElementById('favicon');
+        this.canvas = this.document.createElement('canvas');
+        this.canvas.width = 32;
+        this.canvas.height = 32;
+        this.context = this.canvas.getContext('2d');
+
+        this.img = this.document.createElement('img');
+        this.img.src = this.favicon.href;
+    }
+
+    updateColor(color) {
+        // Defer update if img hasnt loaded
+        if (!this.img.complete) {
+            return this.img.onload = () => this.updateColor(color);
+        }
+        
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.drawImage(this.img, 0, 0, this.canvas.width, this.canvas.height);
+
+        this.context.globalCompositeOperation = "source-atop";
+        this.context.globalAlpha = 1.0;
+        this.context.fillStyle = color;
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.globalCompositeOperation = "source-over";
+        this.context.globalAlpha = 1.0;
+        
+        this.favicon.href= this.canvas.toDataURL("image/x-icon", 1);
+    
+       
+    }
+
+}
--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Theme } from '../model/theme';
 import { Palette, PaletteType } from '../model/palette';
+import { FaviconService } from './favicon.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ThemeService {
-
+  
+  constructor(
+    private favicon: FaviconService,
+  ) {}
+  
   setTheme(theme: Theme) {
     Object.keys(theme).forEach((key) => this.updatePalette(key as PaletteType, theme[key]));
+    this.favicon.updateColor(theme.primary['500']);
   }
 
   private updatePalette(type: PaletteType, palette: Palette) {
@@ -19,3 +25,4 @@ export class ThemeService {
     document.body.style.setProperty(name, value);
   }
 }
+
