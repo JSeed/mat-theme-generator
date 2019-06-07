e29234edab4d4368625d27e72e1e625437565d6c
Jon Seed <jonathan.j.seed@gmail.com>
2019-06-07 02:37:08 +0000

    Prototyping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import WebFont from 'webfontloader';
 
 @Component({
   selector: 'mtb-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  font: string;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.document.body.style.setProperty('--gen-font-family', `'Roboto'`);
+  }
+
+  setFont() {
+    console.log(this.font);
+    WebFont.load({
+      fontactive: (fa) => console.log('fa', fa),
+      google: {
+        families: [this.font]
+      }
+  });
+
+  this.document.body.style.setProperty('--gen-font-family', `'${this.font}'`);
+  }
+
+  fontString() {
+    return `"${this.font}"`;
+  }
+}
